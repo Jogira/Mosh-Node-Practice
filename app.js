1171da84f69d4677a16088ac9e8d315acac45651
Jonathan <Zweitonegoismus@gmail.com>
2021-05-20 15:57:06 -0400

    Chapter 2

--- app.js
+++ app.js
@@ -1,12 +1,78 @@
-const EventEmitter = require('events');
 
 
-const Logger = require('./logger');
-const logger = new Logger();
-//Register a listener.
-logger.on('messageLogged', (arg) =>{
-    console.log('Listener called', arg);
-});
+
+//-------------------------------Chapter 1 and 2--------------------
+const path = require('path');
+
+var pathObj = path.parse(__filename);
+console.log(pathObj);
 
 
-logger.log('message');
+// const os = require('os');
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
+
+// //console.log('Total Memory: ' + totalMemory);
+// console.log(`Total Memory: ${totalMemory}`);
+// console.log(`Free Memory: ${freeMemory}`);
+
+
+
+// var sayHello = function()
+// {
+
+// }
+
+// window.sayHello();
+
+
+// const log = require('./logger');
+// log('message');
+
+
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/') {
+//         res.write('Hello World')
+//         res.end();
+//     }
+
+//     if (req.url === '/api/courses') {
+//         res.write(JSON.stringify([1,2,3]));
+//         res.end();
+//     }
+// });
+// server.listen(3000);
+
+// console.log('Listening on port 3000...');
+
+
+
+
+// const fs = require('fs');
+
+// //const files = fs.readdirSync('./');
+// //console.log(files);
+
+// fs.readdir('.$', function(err,files) {
+
+//     if (err) console.log('Error', err);
+//     else console.log('Result', files);
+// });
+
+
+
+
+// No reconst EventEmitter = require('events');
+
+
+// const Logger = require('./logger');
+// const logger = new Logger();
+// //Register a listener.
+// logger.on('messageLogged', (arg) =>{
+//     console.log('Listener called', arg);
+// });
+
+
+// logger.log('message');
--- logger.js
+++ logger.js
@@ -1,20 +1,36 @@
-const EventEmitter = require('events');
-
-var url = 'http://mylogger.io/log';
 
 
-class Logger extends EventEmitter
-{ 
-    log(message)
-    {
-        //Send HTTP request.
-        console.log(message);
+
+
+
+// console.log(__filename);
+// console.log(__dirname);
+
+// var url = 'http://mylogger.io/log';
+// function log(message) {
+//     //Send and HTTP request.
+//     console.log(message);
+// }
+// module.exports = log;
+
+
+
+
+// const EventEmitter = require('events');
+
+// var url = 'http://mylogger.io/log';
+
+
+// class Logger extends EventEmitter
+// { 
+//     log(message)
+//     {
+//         //Send HTTP request.
+//         console.log(message);
     
-        //Raise an event.
-        this.emit('messageLogged', {id: 1, url: 'http://'});
-    }
-}
+//         //Raise an event.
+//         this.emit('messageLogged', {id: 1, url: 'http://'});
+//     }
+// }
 
-
-
-module.exports = Logger;
+// module.exports = Logger;
