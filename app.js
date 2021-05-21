0941e8751be2309b36c713f05d92b58e1220d0e7
Jonathan <Zweitonegoismus@gmail.com>
2021-05-21 17:47:30 -0400

    Chapter 4 Building Restful API

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World')
+        res.end();
+    }
+
+    if (req.url === '/api/courses') {
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
+});
+server.listen(3000);
+
+console.log('Listening on port 3000...');
 
 
 //-------------------------------Chapter 1 and 2--------------------
-const path = require('path');
+// const path = require('path');
 
-var pathObj = path.parse(__filename);
-console.log(pathObj);
+// var pathObj = path.parse(__filename);
+// console.log(pathObj);
 
 
 // const os = require('os');
