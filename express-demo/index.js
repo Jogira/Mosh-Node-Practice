d74cee1ce4089c0901f3310c916ec24e1a900592
Jonathan <Zweitonegoismus@gmail.com>
2021-05-22 14:17:21 -0400

    Chapter 5 Express Advanced Topics

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,79 +1,43 @@
+const helmet = require('helmet');
+const startupDebugger = require('debug')('app:startup');
+const dbDebbuger = require('debug')('app:db');
+const morgan = require('morgan');
 const express = require('express');
 const Joi = require('joi');
+const logger = require('./middleware/logger');
 const app = express();
+const courses = require('./routes/courses');
+const config = require('config');
+const home = require('./routes/home')
+
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.use(express.json());
-
-const courses = 
-[
-    { id: 1, name: 'course1' },
-    { id: 2, name: 'course2' },
-    { id: 3, name: 'course3' },
-];
-
-app.get('/', (req,res) => {
-    res.send('Hello World');
-});
-
-app.get('/api/courses', (req,res) => {
-    res.send(courses);
-});
-
-app.post('/api/courses', (req, res) => {
-    //Validate
-    const { error } = validateCourse(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    
-    const course = 
-    {
-        id: courses.length + 1,
-        name: req.body.name,
-    };
-    courses.push(course);
-    res.send(course);
-});
+app.use(express.urlencoded( {extended: true}));
+app.use(logger);
+app.use(express.static('public'));
+app.use(helmet());
+app.use('/api/courses', courses);
+app.use('/', home);
 
 
-app.get('/api/courses/:id', (req,res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('The course with the given ID is not found.');
-    res.send(course);
-});
+//Configuration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Password: ' + config.get('mail.password'));
 
-app.put('/api/courses/:id', (req,res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('The course with the given ID is not found.');
-
-    //Validate
-    const { error } = validateCourse(req.body);
-
-    if (error) return res.status(400).send(error.details[0].message);
-    //Update Course
-    course.name = req.body.name;
-    res.send(course);
-    //Return
-});
-
-function validateCourse(course)
+if (app.get('env') === 'development')
 {
-    const schema = Joi.object
-    ({
-        name: Joi.string().min(3).required()
-    });
-
-    return schema.validate(course);
+    app.use(morgan('tiny'));
+    startupDebugger('Morgan Enabled..');
 }
 
-app.delete('/api/courses/:id', (req, res) => 
-{
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send('The course with the given ID is not found.');
+dbDebbuger('Connected to the database...');
 
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-    
-    res.send(course);
-
+app.use(function(req,res,next) {
+    console.log('Authenticating...');
+    next();
 });
 
 
